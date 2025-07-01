c38660371289a4dbe6749133c2fd7ae995dabcaf
Elijah Gbogbo <gbogboelijah@gmail.com>
2025-07-01 15:38:15 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const signButton = document.getElementById("signUp-Action");
+
+const signUpNot = () => {
+  alert("Hey, you clicked the sign up button!");
+}
+
+signButton.addEventListener("click", signUpNot);
+
